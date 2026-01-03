511534b9f7debbae05f606c50281393447b80bfe
ECanfa <e.canfarini@gmail.com>
2026-01-03 16:18:25 +0100

    descrizione grafici disturbi complessi

--- code/tank_test_disturbo_dominio_complesso.m
+++ code/tank_test_disturbo_dominio_complesso.m
@@ -7,7 +7,7 @@ run(path_to_constants);
 % Risposta a w(s) riferimento
 W = 3.5;
 bode(W/s*F,{omega_d_min, omega_d_max});
-title('Risposta della funzione ad Anello Chiuso al riferimento w(s)');
+title('Risposta del sistema in Anello Chiuso al riferimento W(s)');
 
 % Check Disturbo (Da controllare)
 A_d = 1.5;
@@ -23,12 +23,11 @@ omega_d_min = 1e-5; %approssimazione verso 0
 omega_d_max = 4.0;
 bode(YY,{omega_d_min, omega_d_max});
 hold on;
-bode(sin_transform,{omega_d_min, omega_d_max})
-title('Trasformata della risposta del sistema ad anello chiuso per un disturbo D(s)');
+bode(sin_transform,{omega_d_min, omega_d_max});
+title('Trasformata della risposta del sistema in anello chiuso per un disturbo D(s)');
+legend('Y_d(s)','D(s)');
 
 % Check Disturbo di Misura
-% (luci)
-% Da controllare, pero il fatto che modulo sia cosi negativo ha senso
 
 A_n = 0.1;
 sin_transform = 0;
@@ -44,4 +43,5 @@ figure(3);
 bode(YY,{omega_n_min, omega_n_max});
 hold on;
 bode(sin_transform,{omega_n_min, omega_n_max})
-title('Trasformata della risposta del sistema ad anello chiuso per un disturbo N(s)');
+title('Trasformata della risposta del sistema in anello chiuso per un disturbo N(s)');
+legend('Y_n(s)','N(s)');
