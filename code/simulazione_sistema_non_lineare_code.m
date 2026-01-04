f15abe9beb34170ff91c740de1bef346b7652f8d
ECanfa <e.canfarini@gmail.com>
2026-01-04 15:39:24 +0100

    grafici non lineare

--- code/simulazione_sistema_non_lineare_code.m
+++ code/simulazione_sistema_non_lineare_code.m
@@ -15,47 +15,59 @@ U = [0 1];
 % a h(xe, ue).
 
 Simulink.sdi.clear;
-W = 1;
+
 
 x1_max_noise = xe(1); % range di valori randomizzati di prova [0, 2xe[
 x2_max_noise = xe(2);
 r = rand([1,5]);
-x1(1:length(r)) = xe(1) + (-x1_max_noise  + 2*r*x1_max_noise);
-x2(1:length(r)) = xe(2) + (-x2_max_noise + 2*r*x2_max_noise);
+x1(1:length(r)) = xe(1) + (-x1_max_noise  + 1.5*r*x1_max_noise);
+x2(1:length(r)) = xe(2) + (-x2_max_noise + 1.5*r*x2_max_noise);
 
 
 load_system("simulazione_sistema_non_lineare");
 mdl = "simulazione_sistema_non_lineare";
 
-simIn(1:length(r)) = Simulink.SimulationInput(mdl);
+simIn(1:(length(r))) = Simulink.SimulationInput(mdl);
 
-for i = length(r):-1:1
+for i = (length(r)):-1:1
     simIn(i) = setModelParameter(simIn(i),"Solver","ode45",...
     "StopTime","20");
     blk = strcat(mdl,"/Constant1");
     simIn(i) = setBlockParameter(simIn(i),blk,"Value","["+x1(1,i)+" "+x2(1,i)+ "]");
+    if i== length(r)+1
+      simIn(i) = setBlockParameter(simIn(i),blk,"Value","["+xe(1)+" "+xe(2)+ "]");
+    end
 end
 
-%out = parsim(simIn, TransferBaseWorkspaceVariables="on");
+Simulink.sdi.clear;
 out = sim(simIn,"UseFastRestart","on");
-
-
-% retrieve outputs from simulations runs
 runIDs = Simulink.sdi.getAllRunIDs;
-for i = length(r):-1:1
+
+figure(1); hold on; grid on;
+
+legend_step = strings(1,length(runIDs)); 
+
+for i = 1:length(runIDs)
     run = Simulink.sdi.getRun(runIDs(i));
     outSignal = getSignalByIndex(run,1);
-    plotOnSubPlot(outSignal,1,1,true)
-end
-Simulink.sdi.view;
 
-bdclose();
+    t = outSignal.Values.Time;
+    %necessario uso di squeeze su outSignal.Values.Data perchè vettore
+    %3D [campione, singleton, valori]
+    y = squeeze(outSignal.Values.Data);
+    y = y(:,1);   % effettivi valori di cui fare il plot
+    legend_step(i) = "x0(1):" + string(x1(1,i)) + " x0(2):" + string(x2(1,i));
+    plot(t,y)
+end
+
+
+%simulazione con ingresso a gradino e stato iniziale uguale allo stato di
+%equilibrio
+legend(legend_step');
 
 %% 
 % Esplorare il range di ampiezza di riferimenti a gradino tali per cui il controllore rimane efficace sul
 % sistema non lineare.
-
-Simulink.sdi.clear;
 
 w_max_step = 1; % range di valori randomizzati di prova [0, 2xe[
 r = rand([1,5]);
@@ -74,14 +86,24 @@ for i = length(r):-1:1
 end
 
 %out = parsim(simIn, TransferBaseWorkspaceVariables="on");
+Simulink.sdi.clear;
 out = sim(simIn,"UseFastRestart","on");
-
-
-% retrieve outputs from simulations runs
 runIDs = Simulink.sdi.getAllRunIDs;
-for i = length(r):-1:1
+
+figure(2); hold on; grid on;
+
+for i = 1:length(runIDs)
     run = Simulink.sdi.getRun(runIDs(i));
     outSignal = getSignalByIndex(run,1);
-    plotOnSubPlot(outSignal,1,1,true)
+
+    t = outSignal.Values.Time;
+    %necessario uso di squeeze su outSignal.Values.Data perchè vettore
+    %3D [campione, singleton, valori]
+    y = squeeze(outSignal.Values.Data);
+    y = y(:,1);  
+    subplot(length(runIDs),1, i);
+   
+    plot(t,y);
+    legend("Ampiezza del gradino "+w(i));
 end
-Simulink.sdi.view; bdclose();
+
